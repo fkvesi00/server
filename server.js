a5567f9c0d744bb7e2dd954894792916eae35072
CofiKingston <74000580+CofiKingston@users.noreply.github.com>
2022-07-29 14:44:19 +0200

    fix

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ const signIn=require('./controllers/signIn');
 const profile=require('./controllers/profile');
 const image=require('./controllers/image');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
     client: 'pg',
     connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },        
       host : '127.0.0.1',
       port : 5432,
       user : 'postgres',
